cd960c650ed133df4db6a429e30841c5fb45734e
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-13 00:02:31 -0500

    Fix most eslint errors

--- src/modules/farm.js
+++ src/modules/farm.js
@@ -5,23 +5,25 @@ farmBack.classList.add('farm');
 document.body.appendChild(farmBack);
 
 const dt = new Date();
-let secs = 6*3600 - dt.getSeconds() + (60 * (dt.getMinutes() + (60 * dt.getHours())));
+let secs = 6 * 3600 - dt.getSeconds() + (60 * (dt.getMinutes() + (60 * dt.getHours())));
 let deg = 0;
 const fps = 30;
-const tickRate = 1000/fps;
+const tickRate = 1000 / fps;
 
 const skyUpdate = () => {
   secs -= tickRate / 360;
   deg = secs;
   sky.style.transform = `rotate(${deg}deg)`;
-}
+};
 skyUpdate();
 
 const tick = () => {
-  secs -= 1/(3600*tickRate);
+  secs -= 1 / (3600 * tickRate);
   skyUpdate();
-}
+};
 
-export const start = () => {
+const start = () => {
   setInterval(tick, tickRate);
-}
+};
+
+export { start };
--- src/modules/sky.js
+++ src/modules/sky.js
@@ -1,57 +1,61 @@
-let sky = document.createElement('div');
+const sky = document.createElement('div');
 sky.classList.add('sky');
-let sun = document.createElement('div');
+const sun = document.createElement('div');
 sun.classList.add('sun');
 
 const newStar = () => {
   const star = document.createElement('div');
   star.classList.add('star');
   return star;
-}
+};
 
 const newCloud = (angle) => {
   const cloud = document.createElement('div');
-  for (let i = Math.round(Math.random()*3); i >= 1; i -= 1) {
+  for (let i = Math.round(Math.random() * 3); i >= 1; i -= 1) {
     const bump = document.createElement('div');
-    let [x, y] = [Math.random()*30, Math.random()*30];
+    const [x, y] = [Math.random() * 30, Math.random() * 30];
+    let color = 'white';
+    color = angle >= 360 || angle < 160 ? 'white' : '#a5a5a5';
+    color = angle > 330 || angle <= 180 ? color : 'gray';
     bump.style.position = 'absolute';
     bump.style.top = `${y / 1.5}px`;
-    bump.style.left = `${(Math.random()*2 - 1) * 1.2 * x}px`;
-    bump.style.width = `${1.1 * Math.max(x,y)/i}px`;
-    bump.style.height = `${1.1 * Math.max(x,y)/i}px`;
+    bump.style.left = `${(Math.random() * 2 - 1) * 1.2 * x}px`;
+    bump.style.width = `${(1.1 * Math.max(x, y)) / i}px`;
+    bump.style.height = `${(1.1 * Math.max(x, y)) / i}px`;
     bump.style.borderRadius = '50%';
     bump.style.backgroundColor = `
-    ${360 <= angle || angle < 160 ? 'white' : ( 330 < angle || angle <= 180 ? '#a5a5a5' : 'gray')}
+    ${color}
     `;
     cloud.appendChild(bump);
   }
   cloud.classList.add('cloud');
   return cloud;
-}
+};
 
 const makeStars = () => {
   for (let i = 0; i < 100; i += 1) {
     const star = newStar();
     star.classList.add('star');
-    star.style.animation = `flicker ${Math.random()*10}s ease alternate infinite`;
+    star.style.animation = `flicker ${Math.random() * 10}s ease alternate infinite`;
     star.style.position = 'absolute';
-    let [x, y] = [Math.random()*100, 60 + Math.random()*40];
+    const [x, y] = [Math.random() * 100, 60 + Math.random() * 40];
     star.style.top = `${y}%`;
     star.style.left = `${x}%`;
-    star.style.width = `${x/16}px`;
-    star.style.height = `${x/16}px`;
+    star.style.width = `${x / 16}px`;
+    star.style.height = `${x / 16}px`;
     star.style.borderRadius = '50%';
     sky.appendChild(star);
   }
-}
-
+};
 
 function makeClouds() {
-  let rad = sky.offsetWidth / 2;
+  const rad = sky.offsetWidth / 2;
   for (let i = 0; i < 160; i += 1) {
-    let angle = Math.random() * 360;
-    let mag = Math.random() * rad * .5 + rad*.4;
-    let [x, y] = [Math.cos(angle * Math.PI / 180) * mag, Math.sin(angle * Math.PI / 180) * mag];
+    const angle = Math.random() * 360;
+    const mag = Math.random() * rad * 0.5 + rad * 0.4;
+    const [x, y] = [
+      Math.cos((angle * Math.PI) / 180) * mag, Math.sin((angle * Math.PI) / 180) * mag,
+    ];
     const cloud = newCloud((angle + 180 + Math.random() * 12 - 6) % 360);
     cloud.classList.add('cloud');
     cloud.style.position = 'absolute';
@@ -63,8 +67,6 @@ function makeClouds() {
     sky.appendChild(cloud);
   }
 }
-
-
 
 sky.appendChild(sun);
 document.querySelector('#game-background').appendChild(sky);
